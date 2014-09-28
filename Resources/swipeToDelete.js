86124ddf35cef1dc3267a6f52536b887b17564d3
Alain Lehmann <alain.lehmann@gmail.com>
2014-09-29 00:06:03 +0200

    improve the table redrawing

--- Resources/swipeToDelete.js
+++ Resources/swipeToDelete.js
@@ -40,9 +40,6 @@ exports.emulateRowSwipeEvents = function (table, SENSITIVITY) {
 };
 
 exports.emulateRowDeleteEvents = function (table, forceIOS) {
-    // popup seems to work stable, while emulating iOS style has redraw/update issues..
-    // => may the redraw problems are just a simulator thing
-    // => currently resolving the problem with a hide/show of the entire table
     var MIMIC_IOS = true;
     var SENSITIVITY = 3;
 
@@ -50,6 +47,16 @@ exports.emulateRowDeleteEvents = function (table, forceIOS) {
         return; // only required on android, built-in for iOS
     }
     exports.emulateRowSwipeEvents(table, SENSITIVITY);
+
+    // popup seems to work stable, while emulating iOS style has redraw/update issues..
+    // => may the redraw problems are just a simulator thing
+    // => currently resolving the problem with a hide/show of the entire table
+    function redrawHack() {
+        // the hide/show is a workaround to enforce screen redraw
+        // - maybe not necessary on the actual device.
+        // - any alternatives?
+        table.hide(); table.show();
+    }
 
     function swipe(e) {
         Ti.API.debug("swipe in direction: "+e.direction);
@@ -113,6 +120,7 @@ exports.emulateRowDeleteEvents = function (table, forceIOS) {
                 row.remove(btn);
                 row = btn = null;
                 Ti.API.info("removed");
+                redrawHack();
                 if (e.source.id==='swipeDelete') {
                     doDelete();
                 }
@@ -121,11 +129,6 @@ exports.emulateRowDeleteEvents = function (table, forceIOS) {
         table.addEventListener('touchstart', buttonRemover);
         row.hasChild = false;
         row.add(btn);
-        btn.animate({width:width, duration:fading}, function() {
-            // the hide/show is a workaround to enforce screen redraw
-            // - maybe not necessary on the actual device.
-            // - any alternatives?
-            table.hide(); table.show();
-        });
+        btn.animate({width:width, duration:fading}, redrawHack);
     }
 }
